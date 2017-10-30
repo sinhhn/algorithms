08c14875570fc0f37944ad2d0a9c7e2078fe44eb
ホアン ゴック シン <h.sinh@bravesoft.co.jp>
2017-10-30 17:57:01 +0900

    add insertion sort

--- src/test/java/basic/InsertionSortTest.java
+++ src/test/java/basic/InsertionSortTest.java
@@ -1,21 +1,22 @@
 package basic;
 
+import basic.sort.InsertionSort;
 import basic.sort.SelectionSort;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class InsertionSortTest {
     @Test
-    void selectionSortTest1() {
+    void insertionSortTest1() {
         Integer[] unsorted = {5, 2, 3, 4, 1, 7, 9};
         Integer[] sorted = {1, 2, 3, 4, 5, 7, 9};
-        Assert.assertArrayEquals(sorted, SelectionSort.selectionSort(unsorted));
+        Assert.assertArrayEquals(sorted, InsertionSort.insertionSort(unsorted));
     }
 
     @Test
-    void selectionSortTest2() {
+    void insertionSortTest2() {
         Integer[] unsorted = {1, 1, 2, 3, 4, 5, 6, 1};
         Integer[] sorted = {1, 1, 1, 2, 3, 4, 5, 6};
-        Assert.assertArrayEquals(sorted, SelectionSort.selectionSort(unsorted));
+        Assert.assertArrayEquals(sorted, InsertionSort.insertionSort(unsorted));
     }
 }
