1a9fcb2379072c64078c810ed7f513e0a0adc085
ホアン ゴック シン <h.sinh@bravesoft.co.jp>
2017-12-15 12:24:14 +0900

    add new algorithm

---
+++ src/main/java/basic/codefight/NonRepeatCharacter.java
@@ -0,0 +1,27 @@
+package basic.codefight;
+
+import java.util.*;
+
+public class NonRepeatCharacter {
+    public static char firstNotRepeatingCharacter(String s) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            boolean flag = true;
+            if(set.contains(s.charAt(i))) {
+                flag = false;
+                continue;
+            }
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j) && i != j) {
+                    flag = false;
+                    set.add(s.charAt(i));
+                    break;
+                }
+            }
+            if (flag) {
+                return s.charAt(i);
+            }
+        }
+        return '_';
+    }
+}
---
+++ src/test/java/basic/codefight/NonRepeatCharacterTest.java
@@ -0,0 +1,19 @@
+package basic.codefight;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class NonRepeatCharacterTest {
+    @Test
+    void firstNonRepeatCharacterTest1() {
+        String s = "abacabad";
+        Assert.assertEquals('c', NonRepeatCharacter.firstNotRepeatingCharacter(s));
+    }
+
+    @Test
+    void firstNonRepeatCharacterTest2() {
+        String s = "abacabaabacaba";
+        Assert.assertEquals('_', NonRepeatCharacter.firstNotRepeatingCharacter(s));
+    }
+
+}
